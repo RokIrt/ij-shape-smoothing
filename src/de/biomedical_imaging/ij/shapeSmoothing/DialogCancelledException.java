86ddd917371e3db13dd5303928c7f7db7af01ef6
wagner.thorsten <wagner.thorsten@gmx.de>
2013-08-08 12:38:13 +0000

    Initial Commit

---
+++ src/de/biomedical_imaging/ij/shapeSmoothing/DialogCancelledException.java
@@ -0,0 +1,8 @@
+package de.biomedical_imaging.ij.shapeSmoothing;
+
+public class DialogCancelledException extends Exception {
+
+	private static final long serialVersionUID = 2897922459651812757L;
+	
+	
+}
